0c3a603a03bf47755662d867b22c276e1bf55cc4
Paulo Ariel Pareja <ppareja@technisys.com>
2019-07-11 17:03:57 -0300

    first commit, incomplete

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const  exphbs = require('express-handlebars');
+const router = require('./routes');
+const path = require('path');
+
+const app = express();
+
+app.engine('handlebars',
+    exphbs({
+        defaultLayout: 'layout'
+    })
+);
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use('/', router());
+
+
+app.listen(5000);
